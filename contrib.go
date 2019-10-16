86a46a1c4f29e53e23af70c8f4479ca2ed996762
tudyzhb <tudyzhb@gmail.com>
2019-10-16 10:30:44 +0800

    chore: logger interface

---
+++ contrib.go
@@ -0,0 +1,29 @@
+package contrib
+
+// Logger 日志输出
+type Logger interface {
+	SetLevel(level uint32)
+	GetLevel() uint32
+	//
+	Debug(args ...interface{})
+	Debugf(format string, args ...interface{})
+	Debugln(args ...interface{})
+	Info(args ...interface{})
+	Infof(format string, args ...interface{})
+	Infoln(args ...interface{})
+	Warn(args ...interface{})
+	Warnf(format string, args ...interface{})
+	Warnln(args ...interface{})
+	Error(args ...interface{})
+	Errorf(format string, args ...interface{})
+	Errorln(args ...interface{})
+	Print(args ...interface{})
+	Printf(format string, args ...interface{})
+	Println(args ...interface{})
+	Fatal(args ...interface{})
+	Fatalf(format string, args ...interface{})
+	Fatalln(args ...interface{})
+	Panic(args ...interface{})
+	Panicf(format string, args ...interface{})
+	Panicln(args ...interface{})
+}
