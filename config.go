2c933feb3092d3560d063131e938475ac8872ebc
tudyzhb <tudyzhb@gmail.com>
2019-10-16 13:37:36 +0800

    feat: PubConfigRead 读取配置文件

--- config.go
+++ config.go
@@ -8,6 +8,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strings"
 	"sync"
 )
@@ -20,6 +21,47 @@ type Config struct {
 	hookChange func(interface{}) error //
 	comments   map[string]string       // 配置文件的备注信息
 	silent     bool                    // true: 不打印日志
+}
+
+// PubConfigRead 读取配置文件
+func PubConfigRead(configPath string, targetConfig interface{}, defaultConfig interface{}) (err error) {
+	var b []byte
+	if b, err = ioutil.ReadFile(configPath); err != nil {
+		if os.IsNotExist(err) == false {
+			return
+		}
+		// 自动创建配置文件
+		if defaultConfig == nil {
+			err = fmt.Errorf(`[config] defaultConfig undefined, "%s" error: %v`, configPath, err)
+			return
+		}
+		if d, _err2 := yaml.Marshal(defaultConfig); _err2 != nil {
+			err = _err2
+			return
+		} else if err = ioutil.WriteFile(configPath, d, 0666); err != nil {
+			return
+		}
+		err = fmt.Errorf(`[config] please modify "%s" and run again`, configPath)
+		return
+	}
+
+	// 读取配置
+	if err = yaml.Unmarshal(b, targetConfig); err != nil {
+		return
+	}
+
+	// 设置保存地址对对象指针
+	type configInterface interface {
+		SetSavePath(savePath string) (err error)
+		SetSavePoint(saveTarget interface{}) (err error)
+	}
+	if _c, _ok := targetConfig.(configInterface); _ok == true {
+		_ = _c.SetSavePath(configPath)
+		_ = _c.SetSavePoint(targetConfig)
+	}
+
+	//
+	return
 }
 
 // toJson
