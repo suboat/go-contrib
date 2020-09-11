1ed8f523e7ca31b1f5ec5ea21d61754eb473b432
tudyzhb <tudyzhb@gmail.com>
2020-09-11 23:38:35 +0800

    chore: update log

--- log/logrus_test.go
+++ log/logrus_test.go
@@ -1,13 +1,50 @@
 package log
 
 import (
+	"os"
 	"testing"
+	"time"
 )
 
+//
 func Test_File(t *testing.T) {
+	RotateTime = time.Second
+	RotateSuffix = ".%Y%m%d%H%M%S"
+	//
 	var (
-		l = NewLogFile("./test.log")
+		a *os.File
+		l = NewLogFile("./log/test.log")
+		//l  = NewLog(nil)
+		tk = time.NewTicker(time.Second)
 	)
-	l.Infof("ha ha")
-	l.Errorf("la la")
+	if false {
+		a, _ = os.OpenFile("./all.log", os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+		LogOut = a
+	} else {
+		LogOut = os.Stderr
+	}
+
+	l.Infof("begin")
+	go func() {
+		i := 0
+		for {
+			select {
+			case <-tk.C:
+				i += 1
+				if i%3 == 0 {
+					l.Errorf("%d", i)
+				} else if i%2 == 0 {
+					l.Copy().Infof("%d", i)
+				} else {
+					l.Debugf("%d", i)
+				}
+
+			}
+		}
+	}()
+
+	Debug("sleep 1")
+	Log.Debug("sleep 2")
+	time.Sleep(time.Second * 7)
+	l.Errorf("end")
 }
